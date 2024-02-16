a268166c9b5778d55b8e926d184d8825fead218a
tarun <tarun.adhikari@cloudwick.com>
2024-02-16 10:13:50 +0530

    add: pre-commit.sh

---
+++ pre-commit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Author Name: Tarun Adhikari
+#Publish Date: Feb 16, 2024
+#Last Updated: Feb 16, 2024
+
+declare -a pref=(
+    [0]=add
+    [1]=update
+    [2]=remove
+    [3]=refactor
+    [4]=bug-fix
+    [5]=document
+)
+
+msg=$(git diff --cached --pretty=format:%s)
+
+if echo "$commit_message" | grep -E "^[[:space:]]*($(IFS="|"; echo "${pref[*]}"))[[:space:]]*:" >/dev/null 2>&1; then
+    echo "Correct message format......."
+    echo "Committing your changes"
+    git commit -m "$msg"
+else
+    echo "Warning: Wrong commit message format"
+    echo "Allowed prefixes list:-"
+    for i in ${pref[@]}
+    do
+        echo $i
+    done
+fi
